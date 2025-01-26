685ea64448cb35443b6c8f7cd20ffafc03e1645a
Monica Mooney <mm5523@drexel.edu>
2025-01-26 12:51:01 -0500

    Added candies practice problem

---
+++ src/main/com/mamoo/CandiesTestObject.java
@@ -0,0 +1,12 @@
+package com.mamoo;
+
+public class CandiesTestObject {
+
+    int[] candies;
+    int extraCandies;
+
+    public CandiesTestObject(int[] candies, int extraCandies) {
+        this.candies = candies;
+        this.extraCandies = extraCandies;
+    }
+}
